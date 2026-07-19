df48de6cd73b896f8ff6ee0fb1b9c35075ab6c46
Andreu Gimenez Bolinches <esdandreu@Andreus-MacBook-Air.local>
2026-07-19 15:05:50 +0200

    fix: remove unused code

--- crates/anymount/src/drivers/windows/mod.rs
+++ crates/anymount/src/drivers/windows/mod.rs
@@ -1,10 +1,8 @@
 pub mod callbacks;
 pub mod error;
 pub mod placeholders;
-pub mod register;
 pub mod windows_driver;
 
 pub(crate) use callbacks::Callbacks;
 pub use error::{Error, Result};
-pub use register::{HydrationPolicy, RegistrationConfig};
 pub use windows_driver::{WindowsDriver, WindowsDriverConfig, WindowsMount};
